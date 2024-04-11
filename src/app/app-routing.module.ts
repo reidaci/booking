683eace4921d8e1bdb834d40bc2ac4f397cdb30e
Rei Daci <reiz.daci@outlook.com>
2024-04-11 12:03:09 +0200

    project init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule)
+  }
 ];
 @NgModule({
   imports: [
