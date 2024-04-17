b181417c25d38b14e5a3b3ffb731c6971b93f332
Rei Daci <reiz.daci@outlook.com>
2024-04-17 09:48:16 +0200

    create /edit-offer page

--- src/app/places/offers/new-offer/new-offer.page.spec.ts
+++ src/app/places/offers/new-offer/new-offer.page.spec.ts
@@ -1,9 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { NewOfferPage } from './new-offer.page';
 
 describe('NewOfferPage', () => {
   let component: NewOfferPage;
   let fixture: ComponentFixture<NewOfferPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewOfferPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NewOfferPage);
