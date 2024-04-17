7102892d56d15fea0e7674edbbecb2ddd844cf1a
Rei Daci <reiz.daci@outlook.com>
2024-04-17 09:46:26 +0200

    create /offers page

--- src/app/places/offers/offer-bookings/offer-bookings.page.spec.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.spec.ts
@@ -1,9 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { OfferBookingsPage } from './offer-bookings.page';
 
 describe('OfferBookingsPage', () => {
   let component: OfferBookingsPage;
   let fixture: ComponentFixture<OfferBookingsPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OfferBookingsPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OfferBookingsPage);
--- src/app/places/offers/offers.page.spec.ts
+++ src/app/places/offers/offers.page.spec.ts
@@ -1,9 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { OffersPage } from './offers.page';
 
 describe('OffersPage', () => {
   let component: OffersPage;
   let fixture: ComponentFixture<OffersPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OffersPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OffersPage);
