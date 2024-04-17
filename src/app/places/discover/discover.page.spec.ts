9551e5d6b0d5dd320f7ac47c4fd7c7f8cce04550
Rei Daci <reiz.daci@outlook.com>
2024-04-17 09:50:11 +0200

    create /discover page

--- src/app/places/discover/discover.page.spec.ts
+++ src/app/places/discover/discover.page.spec.ts
@@ -1,9 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { DiscoverPage } from './discover.page';
 
 describe('DiscoverPage', () => {
   let component: DiscoverPage;
   let fixture: ComponentFixture<DiscoverPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DiscoverPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DiscoverPage);
