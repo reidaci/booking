23da0b3d5f63a92342d92c8ac8e9909b9168d2c9
Rei Daci <reiz.daci@outlook.com>
2024-05-07 10:46:21 +0200

    app routing ( auth)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
   { path: 'places', loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule) },
-  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) }
+  { path: 'bookings', loadChildren: () => import('./bookings/bookings.module').then(m => m.BookingsPageModule) },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  }
 ];
 
 @NgModule({
