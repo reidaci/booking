233b862e35e7c53bf3b02b1a2a04db4a31bdc5be
Rei Daci <reiz.daci@outlook.com>
2024-05-07 10:46:51 +0200

    place page & service

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -5,17 +5,16 @@ import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-place-detail',
   templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  styleUrls: ['./place-detail.page.scss']
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private router: Router, private navCtrl: NavController) {}
 
-  constructor(private router: Router, private navCtrl: NavController) { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
   onBookPlace() {
     // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover')
-
+    this.navCtrl.navigateBack('/places/tabs/discover');
+    // this.navCtrl.pop();
   }
 }
